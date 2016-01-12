7e488448741cc69feec42f880f6185c2cd04bb45
Simeria Ionut <simeriaionut@gmail.com>
2016-01-12 11:27:45 +0100

    Added login with validation
    - added login with validation
    - to do: add connection to db

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AssignmentTracker.Models;
 
 namespace AssignmentTracker.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        [HttpGet]
+        public ViewResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult Index(Login assignmentTracker)
+        {
+            return View("Index", assignmentTracker);
         }
     }
 }
---
+++ Models/Login.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AssignmentTracker.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your password")]
+        public string Password { get; set; }
+    }
+}
